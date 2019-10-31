655be9b1dbf25c1525d5192e612ae053aaeb7e32
Altyn Sh <altynbek.shaidyldaev@gmail.com>
2019-10-31 18:37:35 +0600

    Added Providers and Consumers aka ContextAPI

---
+++ src/components/error-boundary/error-boundary.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import ErrorIndicator from '../error-indicator';
+
+export default class ErrorBoundary extends Component{
+
+    state = {
+        hasError: false
+    };
+
+    componentDidCatch(error, errorInfo) {
+    }
+
+    render () {
+
+        if (this.state.hasError) {
+            return <ErrorIndicator/>
+        }
+
+        return this.props.children;
+
+    }
+};
+
+export default ErrorBoundary;
